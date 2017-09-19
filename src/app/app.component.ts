b51ba9d3408498b2eb011e190d48d01545054125
Oleg <reg.barsuk@gmail.com>
2017-09-19 18:04:00 +0300

    fix some mistakes and make custom values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,17 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
   rForm: FormGroup;
-  post:any;                     // A property for our submitted form
-  description:string = '';
-  name:string = '';
-  titleAlert:string = 'This field is required';
+  post: any;                     // A property for our submitted form
+  description = '';
+  name = '';
+  titleAlert = 'This field is required';
 
   constructor(private fb: FormBuilder) {
 
     this.rForm = fb.group({
       'name': [null, Validators.required],
-      'description': [null, Validators.compose([Validators.required, Validators.minLength(30), Validators.maxLength(500)])],
+      'description': [null, Validators.compose([Validators.required, Validators.minLength(30)])],
       'validate' : ''
     });
 
@@ -28,15 +27,15 @@ export class AppComponent {
 
     this.rForm.get('validate').valueChanges.subscribe(
       (validate) => {
-        if (validate == '1') {
+        if (validate === '1') {
           this.rForm.get('name').setValidators([Validators.required, Validators.minLength(3)]);
-          this.titleAlert = "You need to specify at least 3 characters";
+          this.titleAlert = 'You need to specify at least 3 characters';
         } else {
           this.rForm.get('name').setValidators(Validators.required);
         }
         this.rForm.get('name').updateValueAndValidity();
       }
-    )
+    );
   }
 
   addPost(post) {
