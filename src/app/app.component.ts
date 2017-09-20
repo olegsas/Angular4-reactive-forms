16aa58e7cd9066b97e92c8f571ee210c7c792b2d
Oleg <reg.barsuk@gmail.com>
2017-09-20 10:05:04 +0300

    fix OnInit error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,7 +6,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   rForm: FormGroup;
   post: any;                     // A property for our submitted form
   description = '';
